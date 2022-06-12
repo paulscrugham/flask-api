961b97a59360e11522c131fd2d966138dd9fb14f
paulscrugham <paulscrugham@gmail.com>
2022-06-11 20:58:52 -0700

    first commit

---
+++ boat.py
@@ -0,0 +1,169 @@
+from distutils.log import error
+from flask import Blueprint, request
+from google.cloud import datastore
+import json
+import constants
+
+client = datastore.Client()
+
+bp = Blueprint('boat', __name__, url_prefix='/boats')
+
+@bp.route('', methods=['POST','GET'])
+def boats_get_post():
+    if request.method == 'POST':
+        # Save request info in variable
+        content = request.get_json()
+        # Create new boat entity object
+        new_boat = datastore.entity.Entity(key=client.key(constants.boats))
+        # try/except block to catch a missing attribute
+        try: 
+            new_boat.update({
+                "name": content["name"], 
+                "type": content["type"],
+                "length": content["length"],
+                "loads": []
+                })
+        except(KeyError):
+            return {"Error": "The request object is missing at least one of the required attributes"}, 400
+        # Add new boat to Google Cloud Store
+        client.put(new_boat)
+        # Return the new boat attributes
+        response = {
+            "id": new_boat.key.id,
+            "name": new_boat["name"],
+            "type": new_boat["type"],
+            "length": new_boat["length"],
+            "loads": [],
+            "self": request.base_url + '/' + str(new_boat.key.id)
+        }
+        return response, 201
+    elif request.method == 'GET':
+        query = client.query(kind=constants.boats)
+        q_limit = int(request.args.get('limit', '3'))  # default number of results is 3
+        q_offset = int(request.args.get('offset', '0'))  # default offset is 0
+        b_iterator = query.fetch(limit=q_limit, offset=q_offset)
+        pages = b_iterator.pages
+        results = list(next(pages))
+        # Calculate url of next page if more results exist
+        if b_iterator.next_page_token:
+            next_offset = q_offset + q_limit
+            next_url = request.base_url + "?limit=" + str(q_limit) + "&offset=" + str(next_offset)
+        else:
+            next_url = None
+        # Add id of entity, self URL, and load info to results
+        for boat in results:
+            boat["id"] = boat.key.id
+            boat["self"] = request.base_url + '/' + str(boat.key.id)
+            for load in boat["loads"]:
+                load["self"] = request.host_url + 'loads/' + str(load["id"])
+                
+        output = {"boats": results}
+        # Add url of next page to output
+        if next_url:
+            output["next"] = next_url
+        return json.dumps(output)
+    else:
+        return 'Method not recognized'
+
+@bp.route('/<id>', methods=['DELETE','GET'])
+def boats_put_delete(id):
+    if request.method == 'DELETE':
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key)
+        # Send 404 error if no boat with the requested id exists
+        if not boat:
+            return {"Error": "No boat with this boat_id exists"}, 404
+        # Update the carrier attribute of all loads on this boat
+        for item in boat["loads"]:
+            load_key = client.key(constants.loads, int(item["id"]))
+            load = client.get(key=load_key)
+            load["carrier"] = None
+            client.put(load)
+        # Delete the boat
+        client.delete(boat_key)
+        return '', 204
+    elif request.method == 'GET':
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key)
+        # Send 404 error if no boat with the requested id exists
+        if not boat:
+            return {"Error": "No boat with this boat_id exists"}, 404
+        boat["id"] = boat.key.id  # Add id value to response
+        boat["self"] = request.base_url  # Add boat URL to response
+        # Add load URLs to response
+        for load in boat["loads"]:
+            load["self"] = request.host_url + 'loads/' + load["id"]
+        return json.dumps(boat), 200
+    else:
+        return 'Method not recognized'
+
+@bp.route('/<boat_id>/loads/<load_id>', methods=['PUT','DELETE'])
+def add_delete_load(boat_id,load_id):
+    # Add a load to a boat
+    if request.method == 'PUT':
+        boat_key = client.key(constants.boats, int(boat_id))
+        boat = client.get(key=boat_key)
+        load_key = client.key(constants.loads, int(load_id))
+        load = client.get(key=load_key)
+        # Check if the boat and/or load exists
+        if not boat or not load:
+            return {"Error": "The specified boat and/or load does not exist"}, 404
+        # Check if load is on another boat
+        if load["carrier"]:
+            return {"Error": "The load is already loaded on another boat"}, 403
+        # Add boat to load
+        load["carrier"] = {
+            "id": boat_id,
+            "name": boat["name"]
+        }
+        # Add load to boat
+        boat["loads"].append({
+            "id": load_id,
+            "item": load["item"]
+        })
+        # Update both boat and load
+        client.put(load)
+        client.put(boat)
+        return '', 204
+    elif request.method == 'DELETE':
+        error_msg = {"Error": "No boat with this boat_id is loaded with the load with this load_id"}
+        boat_key = client.key(constants.boats, int(boat_id))
+        boat = client.get(key=boat_key)
+        load_key = client.key(constants.loads, int(load_id))
+        load = client.get(key=load_key)
+        # Check if the boat and/or load exists
+        if not boat or not load:
+            return error_msg, 404
+        for item in boat["loads"]:
+            if item["id"] == load_id:
+                # Remove load from boat
+                boat["loads"].remove(item)
+                client.put(boat)
+                # Update load carrier
+                load["carrier"] = None
+                client.put(load)
+                return '', 204
+        # Return 404 if the load was not found on the boat
+        return error_msg, 404
+    else:
+        return 'Method not recognized'
+
+@bp.route('/<id>/loads', methods=['GET'])
+def get_boat_loads(id):
+    boat_key = client.key(constants.boats, int(id))
+    boat = client.get(key=boat_key)
+    if not boat:
+        return {"Error": "No boat with this boat_id exists"}, 404
+    load_list  = []
+    for item in boat['loads']:
+        load_key = client.key(constants.loads, int(item["id"]))
+        load = client.get(key=load_key)
+        # Remove the carrier attribute from the load
+        load.pop('carrier', None)
+        load["id"] = int(item["id"])
+        load["self"] = request.host_url + 'loads/' + str(item["id"])
+        load_list.append(load)
+    res = {
+        "loads": load_list
+    }
+    return json.dumps(res), 200
---
+++ load.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, request
+from google.cloud import datastore
+import json
+from json2html import *
+import constants
+
+client = datastore.Client()
+
+bp = Blueprint('load', __name__, url_prefix='/loads')
+
+@bp.route('', methods=['POST','GET'])
+def loads_get_post():
+    if request.method == 'POST':
+        content = request.get_json()
+        new_load = datastore.entity.Entity(key=client.key(constants.loads))
+        try:
+            new_load.update({
+                "volume": content["volume"],
+                "carrier": None,
+                "item": content["item"],
+                "creation_date": content["creation_date"]
+                })
+        except(KeyError):
+            return {"Error": "The request object is missing at least one of the required attributes"}, 400
+        client.put(new_load)
+        # Return the new boat attributes
+        response = {
+            "id": new_load.key.id,
+            "volume": new_load["volume"],
+            "carrier": new_load["carrier"],
+            "item": new_load["item"],
+            "creation_date": new_load["creation_date"],
+            "self": request.base_url + '/' + str(new_load.key.id)
+        }
+        return response, 201
+    elif request.method == 'GET':
+        query = client.query(kind=constants.loads)
+        q_limit = int(request.args.get('limit', '3'))
+        q_offset = int(request.args.get('offset', '0'))
+        l_iterator = query.fetch(limit= q_limit, offset=q_offset)
+        pages = l_iterator.pages
+        results = list(next(pages))
+        if l_iterator.next_page_token:
+            next_offset = q_offset + q_limit
+            next_url = request.base_url + "?limit=" + str(q_limit) + "&offset=" + str(next_offset)
+        else:
+            next_url = None
+        for e in results:
+            e["id"] = e.key.id
+            e["self"] = request.base_url + '/' + str(e.key.id)
+        output = {"loads": results}
+        if next_url:
+            output["next"] = next_url
+        return json.dumps(output)
+
+
+@bp.route('/<id>', methods=['DELETE','GET'])
+def loads_put_delete(id):
+    if request.method == 'DELETE':
+        load_key = client.key(constants.loads, int(id))
+        load = client.get(key=load_key)
+        # Send 404 error if no boat with the requested id exists
+        if not load:
+            return {"Error": "No load with this load_id exists"}, 404
+        # Get boat carrying this load
+        if load["carrier"]:
+            boat_key = client.key(constants.boats, int(load["carrier"]["id"]))
+            boat = client.get(key=boat_key)
+            for item in boat["loads"]:
+                if item["id"] == id:
+                    boat["loads"].remove(item)
+                    client.put(boat)
+                    continue
+
+        client.delete(load_key)
+        return '', 204
+    elif request.method == 'GET':
+        load_key = client.key(constants.loads, int(id))
+        load = client.get(key=load_key)
+        # Send 404 error if no boat with the requested id exists
+        if not load:
+            return {"Error": "No load with this load_id exists"}, 404
+        load["id"] = load.key.id  # Add id value to response
+        load["self"] = request.base_url  # Add URL to response
+        # Populate carrier attribute
+        if load["carrier"]:
+            load["carrier"]["self"] = request.host_url + 'boats/' + load["carrier"]["id"]
+        return json.dumps(load), 200
+        
+    else:
+        return 'Method not recogonized'
